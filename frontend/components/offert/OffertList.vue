ed51883a336fb67425a005796718cc2722436738
kacpereqqo <remzak.k@gmail.com>
2023-06-14 12:08:34 +0200

    ✨ feat: infinity scroll on offerts

--- frontend/components/offert/OffertList.vue
+++ frontend/components/offert/OffertList.vue
@@ -6,6 +6,10 @@
       :offert="offert"
       @click="getMoreDetails(offert._id)"
     />
+    <div v-if="pending" class="pending">
+      <LoadingIndicator />
+      <span> Fetching Data </span>
+    </div>
   </div>
 </template>
 
@@ -13,19 +17,20 @@
 const ENV = useRuntimeConfig().public;
 const API_URL = ENV.API_URL;
 const router = useRouter();
+const pending = ref(false);
 const { data: offerts } = await fetchMoreOfferts(0, 10);
 
-let newData;
-const loading = false;
 let currentOffertID = "";
+const limit = 20;
 
-function fetchMoreOfferts(offset: number, limit: number) {
+function fetchMoreOfferts(offset = 0, limit = 20) {
   return useFetch<LeadingOffert[]>(API_URL + "/leading? ", {
     params: {
       offset,
       limit,
     },
     method: "POST",
+    server: true,
   });
 }
 
@@ -39,14 +44,22 @@ function getMoreDetails(OffertID: string) {
   }
 }
 
-function scrollHandler() {
-  // console.log(ENV);
+async function scrollHandler() {
+  console.log(ENV);
   const scrollHeight = document.documentElement.scrollHeight;
   const scrollTop = document.documentElement.scrollTop;
   const clientHeight = document.documentElement.clientHeight;
 
   if (scrollTop + clientHeight >= scrollHeight) {
-    offerts.push(fetchMoreOfferts(0, 10).data);
+    if (pending.value) return;
+    if (!offerts.value) return;
+
+    pending.value = true;
+
+    const { data, error } = await fetchMoreOfferts(offerts.value.length, limit);
+    offerts.value = [...offerts.value, ...(data.value as [])];
+
+    pending.value = false;
   }
 }
 
@@ -64,5 +77,16 @@ onUnmounted(() => {
   flex: 1;
   display: grid;
   grid-gap: 1rem;
+}
+
+.pending {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  gap: var(--spacer-7);
+  font-size: var(--font-size-4);
+  padding: var(--spacer-5);
 }
 </style>
